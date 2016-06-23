875a5c823185e5345eab9a9d8b030783e894fa2d
mikevman7 <michaelviveros@gmail.com>
2016-06-22 22:36:55 -0400

    Add CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var routes = require('./routes/index');
 
@@ -11,6 +12,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', routes);
 
